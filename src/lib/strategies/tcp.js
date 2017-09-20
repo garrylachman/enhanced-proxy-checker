a6fa9f7d8a82c5beeee5c98b38407ff5750db48b
Garry Lachman <garry@lachman.co>
2017-09-21 02:44:07 +0300

    tcp strategy

--- src/lib/strategies/tcp.js
+++ src/lib/strategies/tcp.js
@@ -1,11 +1,44 @@
 /* @flow */
+import { Socket } from 'net';
 import BaseStrategy from './base';
 import type { JobType } from '../data-types/types';
 
 export default class TcpStrategy extends BaseStrategy {
   static strategyName: string = 'tcp-strategy';
-  execute(job: JobType): boolean {
+  async execute(job: JobType) {
     this.logDebug(job);
-    return true;
+    let res;
+    await this.tcpCheck(job.host, job.port, job.timeout)
+      .then((result: boolean) => {
+        res = result;
+      })
+      .catch(err => {
+        this.logError(err);
+        res = false;
+      });
+    this.logDebug('result', res);
+    return res;
+  }
+  async tcpCheck(host: string, port: number, timeout: number) {
+    this.logDebug(`tcp check ${host}:${port}, timeout: ${timeout}`);
+
+    return new Promise((resolve, reject) => {
+      const socket = new Socket();
+      const onError = () => {
+        this.logError('tcp check fail');
+        socket.destroy();
+        return reject(new Error('Host not reachable or timeout'));
+      };
+
+      socket.setTimeout(timeout);
+      socket.on('error', onError);
+      socket.on('timeout', onError);
+
+      socket.connect({ host, port }, () => {
+        this.logDebug('tcp check sucess');
+        socket.end();
+        return resolve(true);
+      });
+    });
   }
 }
